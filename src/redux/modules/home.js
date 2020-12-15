5e369ae8d30a5bf61f9ff0f6b4fd8ebfe6abbb09
chujingxl <chujingxl@gmail.com>
2020-12-15 12:16:42 +0800

    web application redux-thunk

--- src/redux/modules/home.js
+++ src/redux/modules/home.js
@@ -1,5 +1,57 @@
+import { get } from "../../utils/request";
+import url from "../../utils/url";
+
+export const types = {
+  //获取猜你喜欢请求
+  FETCH_LIKES_REQUEST: "HOME/FETCH_LIKES_REQUEST", 
+  //获取猜你喜欢请求成功
+  FETCH_LIKES_SUCCESS: "HOME/FETCH_LIKES_SUCCESS",
+  //获取猜你喜欢请求失败
+  FETCH_LIKES_FAILURE: "HOME/FETCH_LIKES_FAILURE",
+}
+
+export const actions = {
+  loadlikes: () => {
+    return (dispatch, getState) => {
+      dispatch(fetchLikesRequest());
+      return get(url.getProductList(0,10)).then(
+        data => {
+          dispatch(fetchLikesSuccess(data));
+        },
+        error => {
+          dispatch(fetchLikesFailure(error));
+        }
+      )
+    }
+  }
+}
+
+const fetchLikesRequest = () => ({
+  type: types.FETCH_LIKES_REQUEST
+})
+
+const fetchLikesSuccess = (data) => ({
+  type: types.FETCH_LIKES_SUCCESS,
+  data
+})
+
+const fetchLikesFailure = (error) => ({
+  type: types.FETCH_LIKES_FAILURE,
+  error
+})
+
 const reducer = (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    caes types.FETCH_LIKES_REQUEST:
+    //todo
+    caes types.FETCH_LIKES_SUCCESS:
+    //todo
+    caes types.FETCH_LIKES_FAILURE:
+    //todo
+    default:
+      return state;
+  }
+  
 }
 
 export default reducer;
---
+++ src/utils/request.js
@@ -0,0 +1,40 @@
+const headers = new Headers({
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+})
+
+function get(url) {
+  return fetch(url, {
+    method: "GET",
+    headers: headers
+  }).then(response => {
+    handleResponse(response, url);
+  }).catch(err => {
+    console.error(`Request failed. URL= ${url}. Message=${err}`)
+    return Promise.reject({error: {message: "Request failed."}})//为了让response继续被调用，即使在异常的情况下也继续返回promise结构。这里直接生成一个reject状态的promise
+  })
+}
+
+function post(url, data) {
+  return fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: data
+  }).then(response => {
+    handleResponse(response, url);
+  }).catch(err => {
+    console.error(`Request failed. URL= ${url}. Message=${err}`)
+    return Promise.reject({error: {message: "Request failed."}})
+  })
+}
+
+function handleResponse(response, url) {
+  if(response.status == 200) {
+    return response.json();
+  } else {
+    console.error(`Request failed. URL= ${url}`)
+    return Promise.reject({error: {message: "Request failed due to server error"}})
+  }
+}
+
+export {get, post}
