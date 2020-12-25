c18312a44ca179386f26e3709a0010d36793c3b9
chujingxl <chujingxl@gmail.com>
2020-12-25 17:11:37 +0800

    Search: reducer

--- src/redux/modules/search.js
+++ src/redux/modules/search.js
@@ -1,7 +1,7 @@
 import url from '../../utils/url';
 import { FETCH_DATA } from '../middleware/api';
-import { schema as keywordSchema} from './entities/keywords';
-
+import { schema as keywordSchema } from './entities/keywords';
+import { combineReducers } from 'redux';
 
 const initialState = {
   inputText: '',
@@ -18,7 +18,7 @@ const initialState = {
    * }
    * 
   */
-  relatedKeywords:{
+  relatedKeywords: {
 
   },
   historyKeywords: []
@@ -26,35 +26,35 @@ const initialState = {
 
 export const types = {
   FETCH_POPULAR_KEYWORDS_REQUEST:
-  'SEARCH/FETCH_POPULAR_KEYWORDS_REQUEST',
+    'SEARCH/FETCH_POPULAR_KEYWORDS_REQUEST',
   FETCH_POPULAR_KEYWORDS_SUCCESS:
-  'SEARCH/FETCH_POPULAR_KEYWORDS_SUCCESS',
+    'SEARCH/FETCH_POPULAR_KEYWORDS_SUCCESS',
   FETCH_POPULAR_KEYWORDS_FAILURE:
-  'SEARCH/FETCH_POPULAR_KEYWORDS_FAILURE',
+    'SEARCH/FETCH_POPULAR_KEYWORDS_FAILURE',
 
-  SET_INPUT_TEXT: 
-  'SEARCH/SET_INPUT_TEXT',
+  SET_INPUT_TEXT:
+    'SEARCH/SET_INPUT_TEXT',
   CLEAR_INPUT_TEXT:
-  'SEARCH/CLEAR_INPUT_TEXT',
+    'SEARCH/CLEAR_INPUT_TEXT',
 
   FETCH_RELATED_KEYWORDS_REQUEST:
-  'SEARCH/FETCH_RELATED_KEYWORDS_REQUEST',
+    'SEARCH/FETCH_RELATED_KEYWORDS_REQUEST',
   FETCH_RELATED_KEYWORDS_SUCCESS:
-  'SEARCH/FETCH_RELATED_KEYWORDS_SUCCESS',
+    'SEARCH/FETCH_RELATED_KEYWORDS_SUCCESS',
   FETCH_RELATED_KEYWORDS_FAILURE:
-  'SEARCH/FETCH_RELATED_KEYWORDS_FAILURE',
+    'SEARCH/FETCH_RELATED_KEYWORDS_FAILURE',
 
   ADD_HISTORY_KEYWORD:
-  'SEARCH/ADD_HISTORY_KEYWORD',
+    'SEARCH/ADD_HISTORY_KEYWORD',
   CLEAR_HISTORY_KEYWORDS:
-  'SEARCH/CLEAR_HISTORY_KEYWORDS'
+    'SEARCH/CLEAR_HISTORY_KEYWORDS'
 }
 
 export const actions = {
   loadPopularKeywords: () => {
     return (dispatch, getState) => {
-      const {ids} = getState().search.popularKeywords;
-      if(ids.length > 0) {
+      const { ids } = getState().search.popularKeywords;
+      if (ids.length > 0) {
         return null;
       }
       const endpoint = url.getPopularKeywords();
@@ -64,7 +64,7 @@ export const actions = {
   loadRelatedKeywords: (text) => {
     return (dispatch, getState) => {
       const { relatedKeywords } = getState.search;
-      if(relatedKeywords[text]){
+      if (relatedKeywords[text]) {
         return null
       }
       const endpoint = url.getRelatedKeywords(text)
@@ -75,9 +75,9 @@ export const actions = {
     type: types.SET_INPUT_TEXT,
     text
   }),
-  clearInputText: () => {
+  clearInputText: () => ({
     type: types.CLEAR_INPUT_TEXT
-  },
+  }),
   addHistoryword: keywordId => ({
     type: types.ADD_HISTORY_KEYWORD,
     text: keywordId
@@ -111,3 +111,90 @@ const fetchRelatedKeywords = (text, endpoint) => ({
   },
   text
 })
+
+const popularKeywords = (state = initialState.popularKeywords, action) => {
+  switch (action.type) {
+    case types.FETCH_POPULAR_KEYWORDS_REQUEST:
+      return { ...state, isFetching: true };
+    case types.FETCH_POPULAR_KEYWORDS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        ids: state.ids.concat(action.response.ids)
+      };
+    case types.FETCH_POPULAR_KEYWORDS_FAILURE:
+      return {
+        ...state,
+        isFetching: false
+      };
+    default:
+      return state;
+  }
+}
+
+const relatedKeywords = (state = initialState.relatedKeywords, action) => {
+  switch (action.type) {
+    case types.FETCH_RELATED_KEYWORDS_REQUEST:
+    case types.FETCH_RELATED_KEYWORDS_SUCCESS:
+    case types.FETCH_RELATED_KEYWORDS_FAILURE:
+      return {
+        ...state,
+        [action.text]: relatedKeywordsByText(state[action.text], action)
+      }
+    default:
+      return state;
+  }
+}
+
+//这是比relatedKeywords更低层的一个reducer，其处理逻辑和popularKeywords一样。
+//state层级越深，reducer处理起来就越麻烦。所以尽量保持state扁平化。
+const relatedKeywordsByText = (state = { isFetching: false, ids: [] }, action) => {
+  switch (action.type) {
+    case types.FETCH_POPULAR_KEYWORDS_REQUEST:
+      return { ...state, isFetching: true }
+    case types.FETCH_RELATED_KEYWORDS_SUCCESS:
+      return { ...state, isFetching: false, ids: state.ids.concat(action.response.ids) }
+    case types.FETCH_RELATED_KEYWORDS_FAILURE:
+      return { ...state, isFetching: false };
+    default:
+      return state;
+  }
+}
+
+const inputText = (state = initialState.inputText, action) => {
+  switch(action.type) {
+    case types.SET_INPUT_TEXT:
+      return action.text;
+    case types.CLEAR_INPUT_TEXT:
+      return '';
+    default:
+      return state
+  }
+}
+
+const historyKeywords = (state = initialState.historyKeywords, action) => {
+  switch(action.type) {
+    case types.ADD_HISTORY_KEYWORD:
+     const data = state.filter(item => {
+        if(item !== action.text) {
+          return true;
+        }else {
+          return null;
+        }
+      })
+      return [action.text, ...data];
+    case types.CLEAR_HISTORY_KEYWORDS:
+      return [];
+    default:
+      return state;
+  }
+}
+
+const reducer = combineReducers({
+  popularKeywords,
+  relatedKeywords,
+  inputText,
+  historyKeywords
+})
+
+export default reducer;
